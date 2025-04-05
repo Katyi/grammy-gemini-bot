75b0192f91872506e6e4b4cfa2cc36dcafbb2526
Alexandra Egorova <egorova.aleksandra@gmail.com>
2025-04-05 06:16:33 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+import { Bot } from 'grammy';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const BOT_API_SERVER = 'https://api.telegram.org';
+const { TELEGRAM_BOT_TOKEN, GEMINI_API_KEY } = process.env;
+if (!TELEGRAM_BOT_TOKEN || !GEMINI_API_KEY) {
+  throw new Error('TELEGRAM_BOT_TOKEN and GEMINI_API_KEY must be provided!');
+}
+const bot = new Bot(TELEGRAM_BOT_TOKEN);
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({
+  model: 'gemini-1.5-flash',
+  systemInstruction:
+    'You are a Telegram Chatbot. Maintain a friendly tone. Keep responses one paragraph short unless told otherwise. You have the ability to respond to audio and pictures.',
+});
+
+const chat = model.startChat();
+
+bot.command('start', async (ctx) => {
+  const user = ctx.from;
+  const fullName = `${user?.first_name} ${
+    user?.last_name ? user.last_name : ''
+  }`;
+  const prompt = `Welcome user with the fullname ${fullName} in one sentence.`;
+  const result = await chat.sendMessage(prompt);
+  return ctx.reply(result.response.text(), { parse_mode: 'Markdown' });
+});
+
+bot.on('message:text', async (ctx) => {
+  const prompt = ctx.message.text;
+  const result = await chat.sendMessage(prompt);
+  return ctx.reply(result.response.text(), { parse_mode: 'Markdown' });
+});
+
+console.log('Gemini бот запущен!');
+bot.start();
