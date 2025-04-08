26ea8728e6a383471a6267def66544a3ea0398ef
Alexandra Egorova <egorova.aleksandra@gmail.com>
2025-04-08 17:18:27 +0200

    some changes

--- api/webhook.js
+++ api/webhook.js
@@ -49,7 +49,7 @@ export const config = {
 const webhookHandler = async (req, res) => {
   try {
     webhookCallback(bot, 'https')(req, res);
-    // res.status(200).send('OK'); // Respond with 200 OK
+    res.status(200).send('OK'); // Respond with 200 OK
   } catch (error) {
     console.error('Webhook error:', error);
     // return res.status(500).send('Webhook error occurred');
